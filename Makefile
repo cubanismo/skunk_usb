9babade32ef7ad7dfb06e10aa123eb04dfecbffb
James Jones <atari@theinnocuous.com>
2021-02-17 19:12:36 -0800

    Add usbverif.c, a host USB verification program
    usbverif.c builds into usbverif.cof, which when
    run, attempts to initialize bulk storage devices
    on both host USB ports on the Skunkboard, write
    2MB of pre-computed random data to each port
    respectively from the files rand0.bin and
    rand1.bin on the JCP host, then read back 2MB of
    data from each port respectively to the files
    usbdump0.bin and usbdump1.bin on the JCP host,
    then closes the JCP console.
    
    It is expected a script on the JCP host will take
    care of ensuring the source files are correctly
    generated, and verifying the data read back from
    the USB devices on the Skunkboard matches that
    in the source files when the program exits.

--- Makefile
+++ Makefile
@@ -13,19 +13,25 @@ TESTOBJS = testdrv.o skunk.o
 
 DUMPOBJS = usbdump.o skunk.o
 
+VERIFOBJS = usbverif.o skunk.o
+
 OBJS = $(COMMONOBJS) $(TESTOBJS) $(DUMPOBJS)
 
 TESTCOF = testdrv.cof
 DUMPCOF = usbdump.cof
+VERIFCOF = usbverif.cof
 
 include $(JAGSDK)/jaguar/skunk/skunk.mk
 
-PROGS = $(TESTCOF) $(DUMPCOF)
+PROGS = $(TESTCOF) $(DUMPCOF) $(VERIFCOF)
 
 $(TESTCOF): $(COMMONOBJS) $(TESTOBJS)
 	$(LINK) $(LINKFLAGS) -o $@ $^
 
 $(DUMPCOF): $(COMMONOBJS) $(DUMPOBJS)
+	$(LINK) $(LINKFLAGS) -o $@ $^
+
+$(VERIFCOF): $(COMMONOBJS) $(VERIFOBJS)
 	$(LINK) $(LINKFLAGS) -o $@ $^
 
 skunkc.o: $(SKUNKDIR)/lib/skunkc.s
---
+++ usbverif.c
@@ -0,0 +1,65 @@
+// Skunk USB - verify host USB functionality
+#include "skunk.h"
+#include "usb.h"
+
+extern int printf(const char *fmt, ...);
+
+static USBDev dev0, dev1;
+
+void start(void) {
+	char buf[2048];
+	int i;
+
+	skunkRESET();
+	skunkNOP();
+	skunkNOP();
+
+	inithusb();
+	initbulkdev(&dev0, 0);
+	initbulkdev(&dev1, 1);
+
+	skunkFILEOPEN("rand0.bin", 1);
+
+	// Write 2MB of rand0.bin to the USB drive on port 0
+	for (i = 0; i < 1024; i++) {
+		skunkFILEREAD(buf, sizeof(buf));
+		writeblocks(&dev0, i * 4, 4, buf);
+	}
+
+	skunkFILECLOSE();
+
+	skunkFILEOPEN("rand1.bin", 1);
+
+	// Write 2MB of rand1.bin to the USB drive on port 1
+	for (i = 0; i < 1024; i++) {
+		skunkFILEREAD(buf, sizeof(buf));
+		writeblocks(&dev1, i * 4, 4, buf);
+	}
+
+	skunkFILECLOSE();
+
+	skunkFILEOPEN("usbdump0.bin", 0);
+
+	// Dump the 2MB written to the USB drive on port 0 back to usbdump0.bin
+	for (i = 0; i < 1024; i++) {
+		readblocks(&dev0, i * 4, 4, buf);
+		skunkFILEWRITE(buf, sizeof(buf));
+	}
+
+	skunkFILECLOSE();
+
+	// XXX I don't know why this is needed, but the following skunkFILEOPEN()
+	// crashes the Jaguar without it.
+	printf("WAR crash opening second file to write\r\n");
+
+	skunkFILEOPEN("usbdump1.bin", 0);
+
+	// Dump the 2MB written to the USB drive on port 1 back to usbdump1.bin
+	for (i = 0; i < 1024; i++) {
+		readblocks(&dev1, i * 4, 4, buf);
+		skunkFILEWRITE(buf, sizeof(buf));
+	}
+
+	skunkFILECLOSE();
+	skunkCONSOLECLOSE();
+}
